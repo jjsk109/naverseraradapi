58b14f568210c591842975f209a773db8773fa5e
장익순(NickSoon) <39641848+jjsk109@users.noreply.github.com>
2022-09-19 20:43:49 +0900

    d

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 import CryptoJS from 'crypto-js';
 import request from 'request';
 import moment from 'moment';
+import fetch from 'node-fetch'
 
-const method = "GET";
+const method = "POST";
 const BASE_URL = 'https://api.naver.com';
-let api_url = "/customer-links";
+const api_url = "/stat-reports";
 const accessKey = "01000000000cac9bfd3acf681edebc6d0419aef24e4b12ad82857abd34dc447eda34d83fa7";
 const secretKey = "AQAAAAA+GqRsk72wMhabq7m511jvwVE5I0agGeP6HAqgW/oEog==";
 const customerid = "2657655";
 const timestamp = moment().unix();
 const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
 hmac.update(timestamp + '.' + method + '.' + api_url);
-let hash = hmac.finalize();
+const hash = hmac.finalize();
+const nowDate = moment().format('YYYYMMDD');
 
-/**
- * 고객 정보 조회
- */
- let parm = "?type=MYCLIENTS"
-let options = {
+const parm = ""
+const body_object = {
+    "reportTp": "AD",
+    "statDt": nowDate,
+}
+const options = {
   url: `${BASE_URL}${api_url}${parm}`,
+  method: 'POST',
   headers: {
+    'Content-Type': 'application/json',
     'X-Timestamp': timestamp,
     'X-API-KEY': accessKey,
     'X-Customer': customerid,
     'X-Signature':  hash.toString(CryptoJS.enc.Base64),
   },
+  body: JSON.stringify(body_object),
 };
 
 
-request.get(options, function (error, response, body) {
-  const result = JSON.parse(body);
+/*
+request.post(options, function (error, response, body) {
+  console.log(body);
+  const result = body && JSON.parse(body);
   console.log(result[0]);
   if (!error && response.statusCode == 200) {
     // res.writeHead(200, { 'Content-Type': 'text/json;charset=utf-8' });
@@ -39,36 +47,20 @@ request.get(options, function (error, response, body) {
     console.log(error)
   }
 });
-
-parm = ""
-api_url = "/stat-reports";
-hmac.update(timestamp + '.' + method + '.' + api_url);
-hash = hmac.finalize();
-let nowDate = moment().format('YYYYMMDD');
-let body = {
-  'reportTp' : 'EXPKEYWORD',
-  'statDt' : `${nowDate}`,
-}
-options = {
-  url: `${BASE_URL}${api_url}${parm}`,
+*/
+const res = await fetch(`${BASE_URL}${api_url}${parm}`, {
+  method: method,
   headers: {
+    'content-type': 'application/json;',
     'X-Timestamp': timestamp,
     'X-API-KEY': accessKey,
     'X-Customer': customerid,
     'X-Signature':  hash.toString(CryptoJS.enc.Base64),
   },
-  body: JSON.stringify(body),
-};
-console.log(options);
-request.post(options, function (error, response, body) {
-  const result = JSON.parse(body);
-  console.log('/stat-reports',result[0]);
-  console.log(body);
-  if (!error && response.statusCode == 200) {
-    // res.writeHead(200, { 'Content-Type': 'text/json;charset=utf-8' });
-    // res.end(body);
-  } else {    
-    console.log('error = ' + response?.statusCode);
-    console.log(error)
-  }
+  body: JSON.stringify(body_object),
 });
+console.log(res);
+const result = await res.json();
+console.log(result);
+
+
